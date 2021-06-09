007e0816da4fb9ddeed92143492174506c8d29cc
StrangeNoob <sipun63427452@gmail.com>
2021-06-09 08:16:05 +0530

    Working with Advanced Types(2)

--- src/app.ts
+++ src/app.ts
@@ -51,89 +51,89 @@ const storedData = userInput ?? 'DEFAULT';
 
 console.log(storedData);
 
-// type UnknownEmployee = Employee | Admin;
+type UnknownEmployee = Employee | Admin;
 
-// function printEmployeeInformation(emp: UnknownEmployee) {
-//   console.log('Name: ' + emp.name);
-//   if ('privileges' in emp) {
-//     console.log('Privileges: ' + emp.privileges);
-//   }
-//   if ('startDate' in emp) {
-//     console.log('Start Date: ' + emp.startDate);
-//   }
-// }
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log('Name: ' + emp.name);
+  if ('privileges' in emp) {
+    console.log('Privileges: ' + emp.privileges);
+  }
+  if ('startDate' in emp) {
+    console.log('Start Date: ' + emp.startDate);
+  }
+}
 
-// printEmployeeInformation({ name: 'Manu', startDate: new Date() });
+printEmployeeInformation({ name: 'Manu', startDate: new Date() });
 
-// class Car {
-//   drive() {
-//     console.log('Driving...');
-//   }
-// }
+class Car {
+  drive() {
+    console.log('Driving...');
+  }
+}
 
-// class Truck {
-//   drive() {
-//     console.log('Driving a truck...');
-//   }
+class Truck {
+  drive() {
+    console.log('Driving a truck...');
+  }
 
-//   loadCargo(amount: number) {
-//     console.log('Loading cargo ...' + amount);
-//   }
-// }
+  loadCargo(amount: number) {
+    console.log('Loading cargo ...' + amount);
+  }
+}
 
-// type Vehicle = Car | Truck;
+type Vehicle = Car | Truck;
 
-// const v1 = new Car();
-// const v2 = new Truck();
+const v1 = new Car();
+const v2 = new Truck();
 
-// function useVehicle(vehicle: Vehicle) {
-//   vehicle.drive();
-//   if (vehicle instanceof Truck) {
-//     vehicle.loadCargo(1000);
-//   }
-// }
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
 
-// useVehicle(v1);
-// useVehicle(v2);
+useVehicle(v1);
+useVehicle(v2);
 
-// interface Bird {
-//   type: 'bird';
-//   flyingSpeed: number;
-// }
+interface Bird {
+  type: 'bird';
+  flyingSpeed: number;
+}
 
-// interface Horse {
-//   type: 'horse';
-//   runningSpeed: number;
-// }
+interface Horse {
+  type: 'horse';
+  runningSpeed: number;
+}
 
-// type Animal = Bird | Horse;
+type Animal = Bird | Horse;
 
-// function moveAnimal(animal: Animal) {
-//   let speed;
-//   switch (animal.type) {
-//     case 'bird':
-//       speed = animal.flyingSpeed;
-//       break;
-//     case 'horse':
-//       speed = animal.runningSpeed;
-//   }
-//   console.log('Moving at speed: ' + speed);
-// }
+function moveAnimal(animal: Animal) {
+  let speed;
+  switch (animal.type) {
+    case 'bird':
+      speed = animal.flyingSpeed;
+      break;
+    case 'horse':
+      speed = animal.runningSpeed;
+  }
+  console.log('Moving at speed: ' + speed);
+}
 
-// moveAnimal({type: 'bird', flyingSpeed: 10});
+moveAnimal({type: 'bird', flyingSpeed: 10});
 
-// // const userInputElement = <HTMLInputElement>document.getElementById('user-input')!;
-// const userInputElement = document.getElementById('user-input');
+// const userInputElement = <HTMLInputElement>document.getElementById('user-input')!;
+const userInputElement = document.getElementById('user-input');
 
-// if (userInputElement) {
-//   (userInputElement as HTMLInputElement).value = 'Hi there!';
-// }
+if (userInputElement) {
+  (userInputElement as HTMLInputElement).value = 'Hi there!';
+}
 
-// interface ErrorContainer { // { email: 'Not a valid email', username: 'Must start with a character!' }
-//   [prop: string]: string;
-// }
+interface ErrorContainer { // { email: 'Not a valid email', username: 'Must start with a character!' }
+  [prop: string]: string;
+}
 
-// const errorBag: ErrorContainer = {
-//   email: 'Not a valid email!',
-//   username: 'Must start with a capital character!'
-// };
+const errorBag: ErrorContainer = {
+  email: 'Not a valid email!',
+  username: 'Must start with a capital character!'
+};
